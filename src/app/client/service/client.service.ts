1e268f60f59de624ca1db281cdcd66cff3486d6e
mechergui med aziz <mechergui.aziz843@gmail.com>
2023-11-28 20:32:17 +0100

    edit+delete

--- src/app/client/service/client.service.ts
+++ src/app/client/service/client.service.ts
@@ -1,39 +1,66 @@
 import { Injectable } from '@angular/core';
 import { IClient } from '../model/iclient';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
-  private clients:IClient []=[
-    {id:1,nom:'mohamed tounsi',
-    email:'mohamed.tounsi@gmail.com',
-    password:'mohamed1',
-    tel:'22222222'},
-
-    {id:2,nom:'maha ben azzouz',
-    email:'maha.benazzouz@gmail.com',
-    password:'maha122222',
-    tel:'23222222'}
+  private clients : IClient[] = [
+    {
+      id:1,
+      nom: 'Mohamed Tounsi',
+      email : 'mohamed.tounsi@gmail.com',
+      password : 'moh123456789',
+      tel:'98111222'
+    },
+    {
+      id:2,
+      nom: 'Maha Ben Azzouz',
+      email : 'maha.benazzouz@gmail.com',
+      password : 'mah987654',
+      tel:'98333444'
+    }
   ];
+
+  clientsArrayEdited = new Subject<IClient[]>();
+
   constructor() { }
-  getClients=()=>{
-    return [...this.clients];//retourne une copie de tableau original c pas une reference
+
+  getClients = () : IClient[]=>{
+    return [...this.clients];
   }
 
-  getClientById=(id:number)=>{
-    return this.clients.find(client=>client.id===id);
-  }
-  getLastId=()=>{
-    return this.clients[this.clients.length-1].id;
+  getClientById = (id : number)
+            : IClient | undefined=>{
+    return this.clients.find(client =>
+                          client.id === id);
   }
 
-  addClient=(client:IClient)=>{
-    this.clients=[...this.clients,client];
+  getLastId = ()=>{
+    return this.clients[this.clients.length - 1].id;
+  }
+
+  //add
+  addClient = (client : IClient) : void=>{
+    this.clients = [...this.clients, client];
   }
 
   //edit
+  editClient = (client : IClient) : void=>{
+    this.clients = this.clients.map(
+      cl=>cl.id===client.id?client:cl
+    )
+  }
 
   //delete
+  deleteClient = (id : number) : void => {
+    this.clients = this.clients.filter(
+      cl=>cl.id !== id
+    )
+    this.clientsArrayEdited.next([...this.clients]);
+    /*const indice = this.clients.findIndex(cl=>cl.id===id)
+    this.clients.splice(indice, 1)*/
+    //console.log(this.clients);
+  }
 }
